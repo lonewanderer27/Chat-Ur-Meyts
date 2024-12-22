359f98665e0d60fc21452cc60812b77230855df9
lonewanderer27 <adrianejames27@gmail.com>
2024-12-23 06:40:09 +0800

    perf(ContinueRoute): lazy load Continue page for improved performance

--- src/routes/ContinueRoute.tsx
+++ src/routes/ContinueRoute.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react'
+import React,{ lazy } from 'react'
 import { Route, RouteComponentProps } from 'react-router'
-import { hideTabBar, showTabBar } from '../utils/TabBar'
-
-import Continue from '../pages/Continue'
+const ContinuePage = lazy(() => import('../pages/Continue'))
 import { IonRouterOutlet } from '@ionic/react'
 import OTP_2_Continue from '../pages/Continue/OTP_2_Continue'
 import useHideTabs from '../hooks/useHideTabs'
@@ -12,7 +10,7 @@ const ContinueRoute: React.FC<RouteComponentProps> = ({ match }) => {
 
   return (
     <IonRouterOutlet id="continue">
-      <Route path={match.url} component={Continue} exact />
+      <Route path={match.url} component={ContinuePage} exact />
       <Route path={`${match.url}/email/otp`} component={OTP_2_Continue} exact />
     </IonRouterOutlet>
   )
